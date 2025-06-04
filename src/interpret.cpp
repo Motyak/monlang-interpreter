cfd1519f6bb390b62121d34c0da0614d626d7e41
Motyak <m0tyak_@live.fr>
2025-06-04 18:24:15 +0200

    use BoolConst::

--- src/interpret.cpp
+++ src/interpret.cpp
@@ -403,19 +403,16 @@ value_t evaluateValue(const MapLiteral&, const Environment*) {
 }
 
 value_t evaluateValue(const SpecialSymbol& specialSymbol, const Environment* env) {
-    static auto Bool_true = prim_value_t(Bool(true));
-    static auto Bool_false = prim_value_t(Bool(false));
-
     if (specialSymbol.name == "$nil") {
         return nil_value_t();
     }
 
     if (specialSymbol.name == "$true") {
-        return &Bool_true;
+        return BoolConst::TRUE;
     }
 
     if (specialSymbol.name == "$false") {
-        return &Bool_false;
+        return BoolConst::FALSE;
     }
 
     if (!env->contains(specialSymbol.name)) {
