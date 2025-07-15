a99dd3d5d27bd4c0394e0d794dc186c14880f6f2
Motyak <m0tyak_@live.fr>
2025-07-15 21:39:20 +0200

    main.cpp + embed.sh

--- src/main.cpp
+++ src/main.cpp
@@ -9,30 +9,51 @@
 #include <utils/str-utils.h>
 #include <utils/vec-utils.h>
 #include <utils/env-utils.h>
+#include <utils/args-utils.h>
+
+#include <cstring>
 
 #define unless(x) if(!(x))
+
+std::string ARG0;
+std::vector<std::string> SRC_ARGS;
 
 static std::string STDIN_SRCNAME = env_or_default("STDIN_SRCNAME", "<stdin>");
 
 [[noreturn]] int repl_main(int argc, char* argv[]);
 int stdinput_main(int argc, char* argv[]);
 int fileinput_main(int argc, char* argv[]);
+int selfinput_main(int argc, char* argv[]);
+
+class SelfInputException : public std::exception{};
 
 int main(int argc, char* argv[])
 {
     if (auto options = second(split_in_two(argv[0], " -"))) {
         if (options->contains("i")) {
-            INTERACTIVE_MODE = true;
+            ::INTERACTIVE_MODE = true;
         }
     }
 
+    ::ARG0 = argv[0];
+    auto ARGS = split_args_in_two(argc, argv, "--").first;
+    ::SRC_ARGS = split_args_in_two(argc, argv, "--").second;
+
     /* delegate main based on execution mode */
 
-    if (argc == 1) {
-        return repl_main(argc, argv);
+    if (ARGS.size() == 0) {
+        if (::INTERACTIVE_MODE) {
+            return repl_main(argc, argv);
+        }
+        try {
+            return selfinput_main(argc, argv);
+        }
+        catch (const SelfInputException&) {
+            return repl_main(argc, argv);
+        }
     }
 
-    if (*argv[1] == '-') {
+    if (ARGS[0] == "-") {
         return stdinput_main(argc, argv);
     }
 
@@ -137,6 +158,50 @@ int fileinput_main(int argc, char* argv[]) {
     catch (const InterpretError& e) {
         std::cerr << "Runtime error: " << e.what() << "\n";
         reportCallStack(e.callStack, tokens, filename);
+        return 101;
+    }
+
+    return 0;
+}
+
+int selfinput_main(int argc, char* argv[]) {
+    (void)argc;
+    const auto self_file = argv[0];
+
+    char magic_sig[7];
+    uint32_t src_size;
+    char* src = nullptr;
+
+    auto ifs = std::ifstream(self_file, std::ios::binary);
+    unless (ifs.is_open()) throw SelfInputException(); // could be permissions issue, ..
+    ifs.seekg(-(sizeof(magic_sig) + sizeof(src_size)), std::ios::end);
+    unless (!ifs.fail()) throw SelfInputException(); // file is too short to conform
+    ifs.read(magic_sig, sizeof(magic_sig));
+    unless (ifs.gcount() == sizeof(magic_sig)) throw SelfInputException(); // failed to read file (magic_sig)
+    unless (std::strncmp("monlang", magic_sig, sizeof(magic_sig)/sizeof(magic_sig[0])) == 0) throw SelfInputException(); // no magic sig
+    ifs.read((char*)&src_size, sizeof(src_size));
+    unless (ifs.gcount() == sizeof(src_size)) throw SelfInputException(); // failed to read file (src_size)
+    ifs.seekg(-(src_size + sizeof(magic_sig) + sizeof(src_size)), std::ios::end);
+    unless (!ifs.fail()) throw SelfInputException(); // // incorrect src size
+    src = new char[src_size];
+    ifs.read(src, src_size);
+    unless (ifs.gcount() == src_size) throw SelfInputException(); // failed to read file (src)
+
+    Program prog;
+    Tokens tokens;
+    try {
+        prog = parse(src, &tokens);
+    }
+    catch (const ParseError&) {
+        std::cerr << "Parsing error" << std::endl;
+        return 102;
+    }
+    try {
+        interpretProgram(prog);
+    }
+    catch (const InterpretError& e) {
+        std::cerr << "Runtime error: " << e.what() << "\n";
+        reportCallStack(e.callStack, tokens);
         return 101;
     }
 
