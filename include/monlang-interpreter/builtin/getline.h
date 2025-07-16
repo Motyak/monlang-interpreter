8ce930a1bda4837370067589760c1007b8fe7e51
Motyak <m0tyak_@live.fr>
2025-07-16 18:07:17 +0200

    added builtin getline

---
+++ include/monlang-interpreter/builtin/getline.h
@@ -0,0 +1,10 @@
+#ifndef BUILTIN_GETLINE_H
+#define BUILTIN_GETLINE_H
+
+#include <monlang-interpreter/types.h>
+
+namespace builtin {
+    extern const value_t getline;
+}
+
+#endif // BUILTIN_GETLINE_H
---
+++ src/builtin/getline.cpp
@@ -0,0 +1,16 @@
+#include <monlang-interpreter/builtin/getline.h>
+
+/* impl only */
+
+#include <monlang-interpreter/builtin/prim_ctors.h>
+
+#include <iostream>
+
+const value_t builtin::getline __attribute__((init_priority(3000))) = new prim_value_t{prim_value_t::Lambda{
+    IntConst::ZERO,
+    [](const std::vector<FlattenArg>&) -> value_t {
+        std::string line;
+        std::getline(std::cin, line);
+        return std::cin.eof()? nil_value_t() : new prim_value_t{prim_value_t::Str(line)};
+    }
+}};
