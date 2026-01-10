3bee1c65e7e270d617563f453f07dd09fb94f0a2
Motyak <m0tyak_@live.fr>
2026-01-10 17:56:51 +0100

    added builtin scan()

---
+++ include/monlang-interpreter/builtin/scan.h
@@ -0,0 +1,12 @@
+#ifndef BUILTIN_SCAN_H
+#define BUILTIN_SCAN_H
+
+#include <monlang-interpreter/types.h>
+
+#include <iostream>
+
+namespace builtin {
+    extern const value_t scan;
+}
+
+#endif // BUILTIN_SCAN_H
